522aabb7f80f1c0f346ba5e7940680da05dfbde6
its-bismay <2023ugee070@nitjsr.ac.in>
2024-06-21 20:04:16 +0530

    setup paths for front-end

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from './Pages/Login/Login'
+import Signup from './Pages/Signup/Signup'
+
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home/>}/>
+      <Route path='/login' exact element={<Login/>}/>
+      <Route path='/register' exact element={<Signup/>}/>
+    </Routes>
+  </Router>
+)
 
 const App = () => {
   return (
-    <div><Home/></div>
+    <div>{routes}</div>
   )
 }
 
--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-700">
-    Hello world!
-  </h1>
+<>
+Home
+</>
   )
 }
 
-export default Home
+export default Home;
