acaa198ee68e082e1d9adcd858a961bfeff2321f
its-bismay <2023ugee070@nitjsr.ac.in>
2024-06-26 18:17:46 +0530

    route for delete note is added

--- Backend/index.js
+++ Backend/index.js
@@ -229,6 +229,36 @@ app.get("/notes", authenticateToken, async (req,res) => {
     })
  }
 })
+
+// Delete a note
+
+app.delete("/note/delete/:noteId", authenticateToken, async (req,res) => {
+    const noteId = req.params.noteId;
+    const { user } = req.user;
+
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note){
+            return res.status(404).json({
+                error: true,
+                message: "Note not found"
+            })
+        }
+
+        await Note.deleteOne({_id: noteId, userId: user._id})
+
+        return res.status(200).json({
+            error: false,
+            message: "Note deleted successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        })
+    }
+})
 //start the server
 app.listen(8000, () => {
     console.log("serever is running on port 8000")
