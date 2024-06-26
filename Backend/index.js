5e0826b2fe756203442aba0ca8b6f83efbb2d4d4
its-bismay <2023ugee070@nitjsr.ac.in>
2024-06-26 11:59:12 +0530

    backend is setup

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*"
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({
+        data: "hello world"
+    })
+});
+
+app.listen(8000, () => {
+    console.log("serever is running on port 8000")
+});
+
+
+module.exports = app;
